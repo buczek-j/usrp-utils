f9b6cacc5fb662cde23d3a6ff8f934987c152251
buczek-j <buczekjohn@gmail.com>
2021-03-02 09:20:54 -0500

    3 nodes debugging

--- Utils/LayerStack/Layer2.py
+++ Utils/LayerStack/Layer2.py
@@ -21,7 +21,7 @@ class L2_ENUMS(Enum):
 
 
 class Layer2(Network_Layer):
-    def __init__(self, mac_ip, send_ack=None, udp_acks=True, num_frames=5, timeout=0.025, n_retrans=5, debug=False):
+    def __init__(self, mac_ip, send_ack=None, udp_acks=True, num_frames=5, timeout=0.03, n_retrans=5, debug=False):
         '''
         Layer 2 network layer object
         :param mac_ip: string for the usrp mac address fo the current node
