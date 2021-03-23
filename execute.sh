444e4f0cd1fc16cacb920df9565ec0e10f8f1daf
buczek-j <buczekjohn@gmail.com>
2021-03-23 14:19:48 -0400

    underflow degugging

--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
 
-sshpass -p wnesl ssh wines-nuc1@192.168.10.101 "cd ~/Documents/usrp-utils; git pull" & sshpass -p wnesl ssh wines-nuc2@192.168.10.102 "cd ~/Documents/usrp-utils; git pull" & sshpass -p wnesl ssh wines-nuc3@192.168.10.103 "cd ~/Documents/usrp-utils; git pull"
+sshpass -p wnesl ssh wines-nuc1@192.168.10.101 "python3 ~/Documents/usrp-utils/UAV_Node.py --index 0" & sshpass -p wnesl ssh wines-nuc2@192.168.10.102 "python3 ~/Documents/usrp-utils/UAV_Node.py --index 1" & sshpass -p wnesl ssh wines-nuc3@192.168.10.103 "python3 ~/Documents/usrp-utils/UAV_Node.py --index 2"
