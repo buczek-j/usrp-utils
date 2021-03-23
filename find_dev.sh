97b926e458f3a08dc0956000efdc7a60047ab62d
buczek-j <buczekjohn@gmail.com>
2021-03-23 09:08:57 -0400

    debugging csv

--- find_dev.sh
+++ find_dev.sh
@@ -1,12 +1,12 @@
 echo 1
-sshpass -p wnesl ssh wines-nuc1@192.168.10.101 "uhd_find_devices "
+sshpass -p wnesl ssh wines-nuc1@192.168.10.101 "source ~/.bashrc; uhd_find_devices "
 echo 2
-sshpass -p wnesl ssh wines-nuc2@192.168.10.102 "uhd_find_devices "
+sshpass -p wnesl ssh wines-nuc2@192.168.10.102 "source ~/.bashrc; uhd_find_devices "
 echo 3
-sshpass -p wnesl ssh wines-nuc3@192.168.10.103 "uhd_find_devices "
+sshpass -p wnesl ssh wines-nuc3@192.168.10.103 "source ~/.bashrc; uhd_find_devices "
 echo 4
-sshpass -p wnesl ssh wines-nuc4@192.168.10.104 "uhd_find_devices "
+sshpass -p wnesl ssh wines-nuc4@192.168.10.104 "source ~/.bashrc; uhd_find_devices "
 echo 5
-sshpass -p wnesl ssh wines-nuc5@192.168.10.105 "uhd_find_devices "
+sshpass -p wnesl ssh wines-nuc5@192.168.10.105 "source ~/.bashrc; uhd_find_devices "
 echo 6
-sshpass -p wnesl ssh wines-nuc6@192.168.10.106 "uhd_find_devices "
+sshpass -p wnesl ssh wines-nuc6@192.168.10.106 "source ~/.bashrc; uhd_find_devices "
