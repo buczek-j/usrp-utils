f71ea192cf63bf74cb6f152246b3aa72c7db3fb8
buczek-j <buczekjohn@gmail.com>
2021-03-25 09:47:52 -0400

    ssh debugging

--- execute.py
+++ execute.py
@@ -21,7 +21,7 @@ def main():
     '''
     Main execution method
     '''
-    CMD = 'source ~/prefix-3.8/setup_env.sh; python3 ~/Documents/usrp-utils/UAV_Node.py --index '
+    CMD = 'source ~/prefix-3.8/setup_env.sh; python3 ~/Documents/usrp-utils/tests.py ' #   UAV_Node.py --index '
     ssh_connections = []
     threads = []
 
