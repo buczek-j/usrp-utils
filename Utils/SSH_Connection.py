4d4c84683813185c6336ef67bd00ac4e71bef548
buczek-j <buczekjohn@gmail.com>
2021-03-25 09:46:19 -0400

    ssh debugging

--- Utils/SSH_Connection.py
+++ Utils/SSH_Connection.py
@@ -42,10 +42,14 @@ class SSH_Connection():
         '''
         try:
             ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(command)
+            opt = ssh_stdout.readlines()
+            opt="".join(opt)
+            print(opt)
         except Exception as e:
-            sys.stderr.write("SSH connection error: {0}".format(e))
-        sys.stdout.write(ssh_stdout.read().decode("utf-8"))
-        sys.stderr.write(ssh_stderr.read().decode("utf-8"))	
+            # sys.stderr.write("SSH connection error: {0}".format(e))
+            print(e)
+        # sys.stdout.write(ssh_stdout.read().decode("utf-8"))
+        # sys.stderr.write(ssh_stderr.read().decode("utf-8"))	
     
     def __del__(self):
         '''
