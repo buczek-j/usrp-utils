1df63c738b5489ebaa7a9374dc35fd356b4f5f6c
buczek-j <buczekjohn@gmail.com>
2021-04-12 09:56:59 -0400

    print rtt

--- LayerStack/Layer2.py
+++ LayerStack/Layer2.py
@@ -74,6 +74,7 @@ class Layer2(Network_Layer):
         '''
         if pktno == self.sent_pkt_dict[addr]:
             globals()["l2_ack"].set()
+            print('L2 ACK', pktno)
 
 
     def parse_header(self, pkt):
@@ -157,7 +158,6 @@ class Layer2(Network_Layer):
 
             while len(down_packet[(L2_Header_Len+(self.chunk_size*(pkno-1))):]) / (self.chunk_size)>0:   #while chunks left
                 chunk = down_packet[(L2_Header_Len+(self.chunk_size*(pkno-1))):(L2_Header_Len+(self.chunk_size*(pkno)))]
-                globals()["l2_ack"].clear()
                 self.down_queue.put(down_packet[0:L2_Header_Len] + chunk, True)
                 
                 self.sent_pkt_dict[down_packet[2:8]]= pkno
