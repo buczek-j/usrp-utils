b1b40277395461d3cdb5f5c6de48670dd361454a
root <root@uhd-4-test-ubuntu-20.lxd>
2021-04-16 18:40:04 +0000

    fixed transcevier with SDR. now encoding transceiver works.

--- passthrough_test.py
+++ passthrough_test.py
@@ -21,19 +21,20 @@ if __name__ == '__main__':
     rcv_socket.setsockopt(zmq.SUBSCRIBE, b'') # subscribe to topic of all (needed or else it won't work)
 
     rvcd_pkts = 0
-    for pkt_n in range(100):
+    for pkt_n in range(1000):
         #while True:
-        print("sending {}".format(pkt_n))
+        print("sending pkt_n: {}".format(pkt_n))
         socket.send(msg)
         if rcv_socket.poll(10) != 0: # check if there is a message on the socket
             msg = rcv_socket.recv() # grab the message
             rvcd_pkts += 1
-            print(len(msg)) # size of msg
+            #print(len(msg)) # size of msg
             data = np.frombuffer(msg, dtype=np.byte, count=-1) 
-            print(data.tobytes())
+            #print(data.tobytes())
+            print("RCV")
 
         else:
-            time.sleep(0.1) # wait 100ms and try again
+            time.sleep(0.02) # wait 100ms and try again
 
         
     print("received {} packets".format(rvcd_pkts))
