2e349765b50b6e4077a7c6b0277257c8c14a1d29
buczek-j <buczekjohn@gmail.com>
2021-04-28 15:47:19 -0400

    synch code script for internetless copying

--- clone.sh
+++ clone.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# ./mass_scp.sh "./random" "~/Documents"
-
 echo 1
-scp -r ~/Documents/usrp-utils wines-nuc1@192.168.10.101:~/Documents;
+sudo rsync -avv -e ssh ~/Documents/usrp-utils/ wines-nuc1@192.168.10.101:~/Documents/usrp-utils/
 echo 2
-scp -r ~/Documents/usrp-utils wines-nuc2@192.168.10.102:~/Documents;
+sudo rsync -avv -e ssh ~/Documents/usrp-utils/ wines-nuc2@192.168.10.102:~/Documents/usrp-utils/
 echo 3
-scp -r ~/Documents/usrp-utils wines-nuc3@192.168.10.103:~/Documents;
+sudo rsync -avv -e ssh ~/Documents/usrp-utils/ wines-nuc3@192.168.10.103:~/Documents/usrp-utils/
+echo 4
+sudo rsync -avv -e ssh ~/Documents/usrp-utils/ wines-nuc4@192.168.10.104:~/Documents/usrp-utils/
+echo 5
+sudo rsync -avv -e ssh ~/Documents/usrp-utils/ wines-nuc5@192.168.10.105:~/Documents/usrp-utils/
+echo 6
+sudo rsync -avv -e ssh ~/Documents/usrp-utils/ wines-nuc6@192.168.10.106:~/Documents/usrp-utils/
+
+# scp -r ~/Documents/usrp-utils wines-nuc3@192.168.10.103:~/Documents;
 
