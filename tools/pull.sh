89c9b5860ff09932be1609146e097f3b47046b1d
buczek-j <buczekjohn@gmail.com>
2021-06-08 09:58:41 -0400

    update paths

--- tools/pull.sh
+++ tools/pull.sh
@@ -7,6 +7,6 @@ sshpass -p wnesl ssh wines-nuc6@192.168.10.106 "cd ~/Documents/usrp-utils; git s
 echo 4
 sshpass -p wnesl ssh wines-nuc7@192.168.10.107 "cd ~/Documents/usrp-utils; git stash; git pull";
 echo 5
-sshpass -p wnesl ssh wines-nuc7@192.168.10.108 "cd ~/Documents/usrp-utils; git stash; git pull";
+sshpass -p wnesl ssh wines-nuc8@192.168.10.108 "cd ~/Documents/usrp-utils; git stash; git pull";
 echo 6
 sshpass -p wnesl ssh wines-nuc10@192.168.10.110 "cd ~/Documents/usrp-utils; git stash; git pull";
